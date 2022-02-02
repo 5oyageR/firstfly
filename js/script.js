77ca771640cba67937bc7d54a7459387389b4be1
Seva <deaaalmail@gmail.com>
2022-02-02 10:56:17 +0300

    firs commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//const answ = [];
+//answ[0] = prompt(`name?`, ``);
+//answ[1] = prompt(`age?`, ``);
+//answ[2] = prompt(`wife?`, ``);
+//console.log(typeof(answ));
+
+//const  cat = `toys`;
+//console.log(`htttps://url.com/${cat}/5`);
+
+const user = `Seva` ;
+alert(`Hi, ${user}, you are super!`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
